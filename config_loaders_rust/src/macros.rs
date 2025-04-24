5273a09fad6285e7adbeb59c32f5f7ba202d8272
exersalza (Julian J.) <exersalza.dev@gmail.com>
2025-04-24 10:40:20 +0200

    added macros

--- config_loaders_rust/src/macros.rs
+++ config_loaders_rust/src/macros.rs
@@ -1,8 +1,35 @@
-// IDJFKLd
+// Config
+/// Note this macro only works if there's a module called `config` in your src/
+#[macro_export]
+#[allow(clippy::crate_in_macro_def)] // we have to use `crate` here bc we want to use the binary crate not this
+                                     // library
+macro_rules! config {
+    () => {
+        crate::config::conf.lock()
+    };
+}
 
-//#[macro_export]
-//macro_rules! config {
-//    () => {
-//        $crate::toml_loading_with_hot_relaod::conf.lock()
-//    };
-//}
+/// open the config file and return the contents of it parsed to the struct
+#[macro_export]
+macro_rules! open_config {
+    ($path:expr_2021) => {{
+        let _: &str = $path;
+        use std::fs;
+        use toml;
+
+        let contents = match fs::read_to_string($path) {
+            Ok(c) => c,
+            Err(e) => {
+                panic!("Couldn't open config file due to {e}");
+            }
+        };
+
+        match toml::from_str(&contents) {
+            Ok(data) => data,
+            Err(e) => {
+                eprintln!("{e}");
+                panic!("Unable to load data from {}", $path);
+            }
+        }
+    }};
+}
